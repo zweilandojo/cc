b653b1f7eea84536237f67cfb4e1293889317fb6
Zachary Weiland <zacharyweiland@Zacharys-MacBook-Pro.local>
2018-12-25 13:42:55 -0600

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom'
 
 export default () => (
   <header className='Header-header flex bg-white border-b border-grey-light mb-8 p-4'>
-    <h1 className='Header-h1 flex-1 ml-4 md:ml-8'>
+    <h1 className='flex-1 ml-0 md:ml-8'>
       <NavLink exact to='/' className="text-black">Memory</NavLink>
     </h1>
-    <nav className='Header-nav pt-3 mr-4 md:mr-8 flex-1 text-right'>
+    <nav className='Header-nav pt-3 md:mr-8 flex-1 text-right'>
       <NavLink
         to='/create'
         className='Header-navLink px-2'
