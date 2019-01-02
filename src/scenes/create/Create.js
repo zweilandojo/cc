57dbd69a811ad49a2edd00e78749a2e0ba068644
Zak Weiland <zweiland@ZW-OJO.attlocal.net>
2019-01-02 14:18:43 -0600

    create updates

--- src/scenes/create/Create.js
+++ src/scenes/create/Create.js
@@ -4,33 +4,58 @@ import React from 'react'
 
 const Create = () => {
   return (
-    <div className="app">
-      <h1 className="mb-3 text-2xl">Create Memory</h1>
+    <div className="pt-32 max-w-md mx-auto text-white">
+      <h1 className="mb-3 text-4xl text-center">Add a Customer</h1>
       <form className="pt-6 pb-8 mb-2">
         <div className="mb-4">
-          <label className="block text-grey-darker text-sm font-bold mb-2" for="postTitle">
-            Title
+          <label className="block text-sm font-bold mb-2" for="postTitle">
+            Customer Type
           </label>
-          <input placeholder="What's going on?" class="appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="postTitle" type="text" />
+          <select class="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" id="postTitle">
+            <option>Buyer</option>
+            <option>Agent</option>
+            <option>AIT</option>
+          </select>
         </div>
         <div className="mb-4">
-          <label className="block text-grey-darker text-sm font-bold mb-2" for="postTitle">
-            Content
+          <label className="block text-sm font-bold mb-2" for="postTitle">
+            First Name
           </label>
-          <textarea placeholder="" class="appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="postContent">
-          </textarea>
+          <input placeholder="Johnny" class="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" id="postTitle" type="text" />
         </div>
         <div className="mb-4">
-          <label className="block text-grey-darker text-sm font-bold mb-2" for="postTitle">
-            Cover Image
+          <label className="block text-sm font-bold mb-2" for="postTitle">
+            Last Name
+          </label>
+          <input placeholder="Appleseed" class="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" id="postTitle" type="text" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-bold mb-2" for="postTitle">
+            Photo
           </label>
           <input type="file" />
         </div>
         <div className="mb-4">
-          <label className="block text-grey-darker text-sm font-bold mb-2" for="postTitle">
-            Tags
+          <label className="block text-sm font-bold mb-2" for="postTitle">
+            Bio
           </label>
-          <input placeholder="jelly beans, atx, bikes, whatever..." class="appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="postTitle" type="text" />
+          <textarea placeholder="" class="appearance-none border rounded w-full py-2 px-3 eading-tight focus:outline-none focus:shadow-outline" id="postContent">
+          </textarea>
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-bold mb-2" for="postTitle">
+            Age
+          </label>
+          <input placeholder="18" class="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" id="postTitle" type="number" />
+        </div>
+        <div className="mb-8">
+          <label className="block text-sm font-bold mb-2" for="postTitle">
+            Pull Quote
+          </label>
+          <input placeholder="e.g. Was it something I said" class="appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="postTitle" type="text" />
+        </div>
+        <div className="mb-4">
+          <input type="submit" value="Create" className="bg-green text-white text-center py-6 text-xl font-bold tracking-wide uppercase rounded-full w-full" />
         </div>
       </form>
     </div>
